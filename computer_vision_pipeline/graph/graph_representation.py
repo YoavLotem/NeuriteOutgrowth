24e0ae6e416fea4970f3954c4d1704c3a41e754f
YoavLotem <yoavlotem2@gmail.com>
2021-10-23 19:16:00 +0300

    search_mask lowecase

--- computer_vision_pipeline/graph/graph_representation.py
+++ computer_vision_pipeline/graph/graph_representation.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def place_search_disk(neurite_endpoint, search_mask, DISK_MASK):
     """
-    Places the DISK_MASK in the right place in the SEARCH_MASK, that is intended to search for cells in the proximity
+    Places the DISK_MASK in the right place in the search_mask, that is intended to search for cells in the proximity
     of a neurite endpoint, so that the center of the search disk will be at the neurite endpoint.
     This approach to set the SEARCH_MASK using the precomputed DISK_MASK is significantly faster than the naive approach
 
