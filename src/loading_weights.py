192f43c1a825194557bac825aeee964cd473dfc2
YoavLotem <yoavlotem2@gmail.com>
2021-12-05 16:23:59 +0200

    adding the visualizations of the demo

---
+++ src/loading_weights.py
@@ -0,0 +1,23 @@
+import urllib.request
+import shutil
+
+
+mask_rcnn_weights = "https://drive.google.com/file/d/1q6ZWGQewLxw6ZIMBpBmT7FPJ-MMbRsCk/view?usp=sharing"
+
+def download_trained_weights(local_weights_path, verbose=1):
+    """
+    Download deep retina trained weights.
+
+    Parameters
+    ----------
+    local_weights_path: str
+        local path of trained weights
+    verbose: bool
+        bool indicating whether to write messages
+    """
+    if verbose > 0:
+        print("Downloading pretrained model to " + local_weights_path + " ...")
+    with urllib.request.urlopen(mask_rcnn_weights) as resp, open(local_weights_path, 'wb') as out:
+        shutil.copyfileobj(resp, out)
+    if verbose > 0:
+        print("... done downloading pretrained model!")
